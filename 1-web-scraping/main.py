e5c149acceecb8cb64b35a55fc3ab251548c305d
isacpxc <isacpxc@gmail.com>
2025-03-26 10:34:09 -0300

    feat: download and compression

--- 1-web-scraping/main.py
+++ 1-web-scraping/main.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import os
+import zipfile
+import shutil
 
 url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
 
@@ -14,7 +17,7 @@ if response.status_code == 200:
     filtered_links = []
     for tag in internal_links:
         href = tag.get('href', '')  
-        if href.endswith('.pdf') and re.search(r"Anexo_[IVX]+", href):  
+        if href.endswith('.pdf') and re.search(r"Anexo_[IVX]+", href): 
             filtered_links.append(href)
 
     if filtered_links:
@@ -25,3 +28,31 @@ if response.status_code == 200:
         print("Nenhum link com 'Anexo_x' e terminando com '.pdf' foi encontrado.")
 else:
     print(f"Erro ao acessar o site. Código de status: {response.status_code}")
+
+##### download
+
+os.makedirs("temp_pdfs", exist_ok=True)
+
+for link in filtered_links:
+    file_name = link.split("/")[-1]
+    file_path = os.path.join("temp_pdfs", file_name)
+    pdf_response = requests.get(link)
+
+    if pdf_response.status_code == 200:
+        with open(file_path, "wb") as pdf_file:
+            pdf_file.write(pdf_response.content)
+        print(f"Arquivo baixado: {file_name}")
+    else:
+        print(f"Erro ao baixar o arquivo: {file_name}")
+
+zip_file_name = "anexos.zip"
+with zipfile.ZipFile(zip_file_name, "w") as zipf:
+    for root, _, files in os.walk("temp_pdfs"):
+        for file in files:
+            file_path = os.path.join(root, file)
+            zipf.write(file_path, arcname=file)
+
+print(f"Arquivos compactados em: {zip_file_name}")
+
+shutil.rmtree("temp_pdfs")
+
