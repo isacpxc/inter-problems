37065234e8421308004aa8dabbdff42a8950a6dc
isacpxc <isacpxc@gmail.com>
2025-03-26 10:51:35 -0300

    feat: progress bar added

--- 1-web-scraping/main.py
+++ 1-web-scraping/main.py
@@ -4,12 +4,13 @@ import re
 import os
 import zipfile
 import shutil
+from tqdm import tqdm 
 
 url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
 
 try:
     response = requests.get(url)
-    response.raise_for_status()  
+    response.raise_for_status()
 except requests.exceptions.RequestException as e:
     print(f"Erro ao acessar o site: {e}")
     exit()
@@ -39,22 +40,22 @@ else:
     print("Nenhum link com 'Anexo_x' e terminando com '.pdf' foi encontrado.")
     exit()
 
-##### download
+#####download
 try:
     os.makedirs("temp_pdfs", exist_ok=True)
 except Exception as e:
     print(f"Erro ao criar diretório temporário: {e}")
     exit()
 
-for link in filtered_links:
+print("Baixando arquivos PDF...")
+for link in tqdm(filtered_links, desc="Progresso do Download"):
     file_name = link.split("/")[-1]
     file_path = os.path.join("temp_pdfs", file_name)
     try:
         pdf_response = requests.get(link)
-        pdf_response.raise_for_status()  
+        pdf_response.raise_for_status()
         with open(file_path, "wb") as pdf_file:
             pdf_file.write(pdf_response.content)
-        print(f"Arquivo baixado: {file_name}")
     except requests.exceptions.RequestException as e:
         print(f"Erro ao baixar o arquivo {file_name}: {e}")
     except Exception as e:
@@ -62,11 +63,11 @@ for link in filtered_links:
 
 try:
     zip_file_name = "anexos.zip"
+    print("Compactando arquivos em ZIP...")
+    files_to_zip = [os.path.join("temp_pdfs", f) for f in os.listdir("temp_pdfs")]
     with zipfile.ZipFile(zip_file_name, "w") as zipf:
-        for root, _, files in os.walk("temp_pdfs"):
-            for file in files:
-                file_path = os.path.join(root, file)
-                zipf.write(file_path, arcname=file)
+        for file in tqdm(files_to_zip, desc="Progresso da Compactação"):
+            zipf.write(file, arcname=os.path.basename(file))
     print(f"Arquivos compactados em: {zip_file_name}")
 except Exception as e:
     print(f"Erro ao criar o arquivo ZIP: {e}")
